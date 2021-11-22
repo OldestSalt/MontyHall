e27310f098c8699a912bb09cb3cd19becb73d33e
OldestSalt <super.senya2002@yandex.ru>
2021-11-22 21:42:13 +1000

    main

--- index.js
+++ index.js
@@ -5,22 +5,41 @@ const $doors = document.querySelector('#doors-wrap')
 const $text = document.querySelector('#text')
 
 let prizeDoor = 0
+let doors = [1, 2, 3]
+
+function open(door) {
+    door.classList.toggle('opened')
+    if (doors[door] == 1) {
+        door.textContent = 'Win!'
+    }
+    else {
+        door.textContent = 'Empty!'
+    }
+}
 
 function startGame() {
     prizeDoor = Math.floor(Math.random() * 3) + 1
-    let doors = [0, 0, 0]
-    doors[prizeDoor] = 1
+    doors[prizeDoor - 1] = 0
     $text.textContent = 'Choose the door'
 
     $doors.addEventListener('click', (event) => {
         if (event.target == $door1 || event.target == $door2 || event.target == $door3) {
             event.target.style.color = 'red'
-            const chosen = event.target.id[4]
+            const chosen = event.target.id[4] - 1
 
-            if (chosen == prizeDoor) {
-                doors.splice(prizeDoor, 1)
-                
+            if (doors[chosen] == 0) {
+                doors.splice(chosen, 1)
+                open(document.querySelector('#door' + doors[Math.floor(Math.random() * 2)]))
             }
+            else {
+                doors[chosen] = 0
+                doors = doors.filter(i => i != 0)
+                open(document.querySelector('#door' + doors[0]))
+            }
+
+            $text.textContent = 'Now you can change your choice'
+
+            
         }
     })
 }
