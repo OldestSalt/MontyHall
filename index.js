5d8b50151c507be12ef5064ba35705f1de8c5cd4
OldestSalt <super.senya2002@yandex.ru>
2021-11-22 23:39:25 +1000

    main

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const $door2 = document.querySelector('#door2')
 const $door3 = document.querySelector('#door3')
 const $doors = document.querySelector('#doors-wrap')
 const $text = document.querySelector('#text')
+const $again = document.querySelector('#again')
 
-let prizeDoor = 0
-let doors = [1, 2, 3]
+let doors = [], opened = 0, prizeDoor = 0, chosen = -1, finished = false
 
 function open(door) {
-    door.classList.toggle('opened')
-    if (doors[door] == 1) {
+    door.classList.add('opened')
+    if (door.id[4] == prizeDoor) {
         door.textContent = 'Win!'
+        door.setAttribute('style','background-color: yellow !important')
     }
     else {
         door.textContent = 'Empty!'
@@ -18,30 +19,61 @@ function open(door) {
 }
 
 function startGame() {
+    finished = false
+    doors = [1, 2, 3]
     prizeDoor = Math.floor(Math.random() * 3) + 1
+    console.log(prizeDoor)
     doors[prizeDoor - 1] = 0
     $text.textContent = 'Choose the door'
+}
 
-    $doors.addEventListener('click', (event) => {
-        if (event.target == $door1 || event.target == $door2 || event.target == $door3) {
+$doors.addEventListener('click', (event) => {
+    if ((event.target == $door1 || event.target == $door2 || event.target == $door3) && !finished) {
+        if (doors.length == 3) {
             event.target.style.color = 'red'
-            const chosen = event.target.id[4] - 1
+            chosen = event.target.id[4] - 1
 
             if (doors[chosen] == 0) {
                 doors.splice(chosen, 1)
-                open(document.querySelector('#door' + doors[Math.floor(Math.random() * 2)]))
+                opened = doors[Math.floor(Math.random() * 2)]
             }
             else {
                 doors[chosen] = 0
                 doors = doors.filter(i => i != 0)
-                open(document.querySelector('#door' + doors[0]))
+                opened = doors[0]
             }
 
+            open(document.querySelector('#door' + opened))
             $text.textContent = 'Now you can change your choice'
-
-            
         }
-    })
-}
+        else if (event.target.id[4] != opened) {
+            open(event.target)
+            let changed = 'changed'
+            let win = 'win'
+
+            if (event.target.id[4] == chosen + 1) {
+                changed = 'unchanged'
+            }
+            if (event.target.id[4] != prizeDoor) {
+                win = 'lose'
+            }
+
+            finished = true
+
+            document.querySelector(`#${changed} .${win}`).textContent++
+        }
+
+
+    }
+})
+
+$again.addEventListener('click', () => {
+    for (let i = 1; i <= 3; ++i) {
+        document.querySelector('#door' + i).textContent = '?'
+        document.querySelector('#door' + i).classList.remove('opened')
+        document.querySelector('#door' + i).removeAttribute('style')
+    }
+    startGame()
+})
 
 startGame()
